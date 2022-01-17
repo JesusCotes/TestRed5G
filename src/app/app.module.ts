f841ccbf19a6f6e8d6668a6bb6bd9f3e1a87638a
JesusCotes <jmcgarcia93@gmail.com>
2022-01-17 11:41:06 -0500

    Feature: Making Test Red5G

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChoiceFormComponent } from './choice-form/choice-form.component';
+import { FormularyComponent } from './formulary/formulary.component';
+
+const routes: Routes =[
+  { path: '', component: ChoiceFormComponent },
+  { path: 'formulary', component: FormularyComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoiceFormComponent,
+    FormularyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+ RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/choice-form/choice-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choice-form',
+  templateUrl: './choice-form.component.html',
+  styleUrls: ['./choice-form.component.css']
+})
+export class ChoiceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
